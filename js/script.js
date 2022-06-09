1258efd07b1d2b244b7700ebd11e2b60d5db12e8
Bruno <bruno@delucca.cl>
2022-06-09 14:14:54 +0200

    Finished Test Statista

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log('External script implemented successfully'); // Just for debugging, delete later
+
+for (let index = 1; index < 101; index++) {
+  add_fizz_buzz(index);
+}
+
+function add_fizz_buzz (num) {
+  const newLi = document.createElement("li");
+  newLi.classList.add("fizzBuzzList_ul_li");
+  let content_li;
+  
+  // Check which content to add
+  if (num % 3 == 0 && num % 5 == 0) {
+      content_li = document.createTextNode("FizzBuzz");
+  } else if (num % 3 == 0) {
+      content_li = document.createTextNode("Fizz");
+  } else if (num % 5 == 0) {
+      content_li = document.createTextNode("Buzz");
+  } else {
+      content_li = document.createTextNode(num);
+  }
+
+  newLi.appendChild(content_li);
+
+  // add the li to the DOM
+  const fizzBuzzList_ul = document.getElementsByClassName("fizzBuzzList_ul")[0];
+  fizzBuzzList_ul.appendChild(newLi);
+
+}
